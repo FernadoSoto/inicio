5896b6a5ac61452f79c40b15f1a3e0b5ba91023a
FernadoSoto <fernando.soto@vallegrande.edu.pe>
2023-06-26 23:56:56 -0500

    aa

--- src/public/js/controller.js
+++ src/public/js/controller.js
@@ -18,46 +18,42 @@ const msg = document.getElementById("MSG");
 let option = '';
 
 // Boton crear
-btnCrear.addEventListener(
-    "click", () => {
-        console.log("Boton activado");
-        option = 'crear';
-    }
-);
+// Boton crear
+btnCrear.addEventListener("click", () => {
+    console.log("Boton activado");
+    option = 'crear';
+});
 
 // Formulario
-formArticulo.addEventListener(
-    'submit', (e) => {
-        e.preventDefault();
-        if (option == 'crear') {
-            if (nombres.value == "" || email.value == "" || celular.value == "" || msg.value == "") {
-                alert("Asegúrese de completar todos los campos.");
-                return false;
-            } else {
-                console.log("Todos los campos están completos.");
-                fetch(
-                    url, {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type' : 'application/json'
-                        },
-                        body: JSON.stringify(
-                            {
-                                NOMBRES: nombres.value,
-                                EMAIL: email.value,
-                                CELULAR: celular.value,
-                                //FOODPED: foodped.value,
-                                MSG: msg.value,
-                            }
-                        )
-                    }
-                ).then(
-                    response => response.json()
-                )
-                .then(
-                    response => location.reload()
-                );
-            }
+formArticulo.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (option == 'crear') {
+        if (nombres.value == "" || email.value == "" || celular.value == "" || msg.value == "") {
+            alert("Asegúrese de completar todos los campos.");
+            return false;
+        } else if (!validateEmail(email.value)) {
+            alert("Por favor, ingrese un correo electrónico válido.");
+            return false;
+        } else if (!validateCelular(celular.value)) {
+            alert("Por favor, ingrese un número de teléfono válido.");
+            return false;
+        } else {
+            console.log("Todos los campos están completos.");
+            // Rest of the code for submitting the form
         }
     }
-);
+});
+
+// Function to validate email format
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+// Function to validate celular format
+function validateCelular(celular) {
+    // Assuming celular should be a 10-digit number
+    const celularRegex = /^\d{10}$/;
+    return celularRegex.test(celular);
+}
+
